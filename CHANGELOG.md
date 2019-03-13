b9b892db776b7e1fa11e8baec37aeee5d89c7709
Keith E. Truesdell <keithetruesdell@gmail.com>
2019-03-12 22:46:53 -0400

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [0.1.0]	2019-03-12
+----------------------
+* Initial commit - port from widget-counter (other project)
--- spec/angelic-warfare-for-atom-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AngelicWarfareForAtomView from '../lib/angelic-warfare-for-atom-view';
-
-describe('AngelicWarfareForAtomView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
